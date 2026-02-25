4166d5327df7ab10a4e61901142f5ca14f583fa9
JohnCGT <142409850+JohnCGT@users.noreply.github.com>
2026-02-25 19:45:48 +0800

    changed to sectexam url

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://sectexam.app',
         changeOrigin: true,
       },
       '/sanctum': {
-        target: 'http://localhost:8000',
+        target: 'https://sectexam.app',
         changeOrigin: true,
       },
     },
